8ea3e09ba3dd74d86b22904ff679fb4638bad47d
Chen Yu Pao <windperson@gmail.com>
2019-11-19 14:46:35 +0800

    rename invoke project
    update prompt text
    
    Update prompt text

--- AutomationDemo/Program.cs
+++ AutomationDemo/Program.cs
@@ -3,19 +3,20 @@ using System;
 using OpenQA.Selenium.Chrome;
 using BrowserAutomationLib;
 
-namespace GestureDemo
+namespace AutomationDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Begin Google Chrome browser test.");
             using IWebDriver driver = new ChromeDriver();
 
             NavigateDemo.DoBrowsing(driver);
             
             driver.Close();
 
-            Console.WriteLine("done, press any key to close.");
+            Console.WriteLine("Test Done, press any key to close.");
             Console.ReadKey();
         }
     }
